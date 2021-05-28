872dd789573ac371f178ff2f2d6f78f0f580b111
alisamadzadeh46 <alisamadzadeh46@gmail.com>
2021-05-28 23:16:31 +0430

    update

--- account/views.py
+++ account/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .form import *
+from .models import Profile
 
 
-class Login(View):
+class Register(View):
+    form_class = UserRegistrationForm
+    template_name = 'account/login.html'
+
     def get(self, request):
-        return render(request, 'account/login.html')
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
 
-
-
-
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            Profile.objects.create(user=user)
+            messages.success(request, 'حساب کاربری با موفقیت ساخت شد.لطفا وارد شوید', 'info')
+            return redirect('account:register')
+        return render(request, self.template_name, {'form': form})
